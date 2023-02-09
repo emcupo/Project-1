e89e31925af091098ef7eb5b26cf86be3b1c41ba
AlexU2001 <77464396+AlexU2001@users.noreply.github.com>
2023-02-08 21:14:46 -0500

    Synced animation with movement

--- Project 1/Assets/Scripts/PlayerMovement.cs
+++ Project 1/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Initialize")]
     private Rigidbody2D _rb;
+    private Animator _animator;
 
     [Header("Settings")]
     [SerializeField] private float _speed = 1f;
@@ -14,14 +15,21 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         if (_rb == null)
-        {
             _rb = GetComponent<Rigidbody2D>();
-        }
+
+        if (_animator == null)
+            _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         _rb.velocity = _movementInput * _speed;
+        if (_animator != null)
+        {
+            _animator.SetFloat("Horizontal", _movementInput.x);
+            _animator.SetFloat("Vertical", _movementInput.y);
+        }
+
     }
 
     private void OnMove(InputValue inputValue)
