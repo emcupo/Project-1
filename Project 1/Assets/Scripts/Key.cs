e63331250501385b18200cb1627999ea84e1e8b2
JuniorJ2002 <junior_julca@bloomfield.edu>
2023-02-15 11:14:21 -0500

    Level 3
    Created and added in a third level for the game.

---
+++ Project 1/Assets/Scripts/Key.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("key picked up");
+
+            door.GetComponent<BoxCollider2D>().enabled = false;
+            door.SetActive(false);
+
+            this.gameObject.SetActive(false);
+        }
+    }
+}
