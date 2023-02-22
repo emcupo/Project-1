a0a10ef7107282461c87068040c0d2618cd9b58c
AlexU2001 <77464396+AlexU2001@users.noreply.github.com>
2023-02-22 14:00:40 -0500

    Fixed some collision issues

--- Project 1/Assets/Scripts/ChangeScene.cs
+++ Project 1/Assets/Scripts/ChangeScene.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] private bool isActive = true;
-    void OnCollisionEnter2D(Collision2D collider)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
         if (isActive)
         {
