5bc89f74ef2a2708abe3302c7fcd71c3ffe8f556
AlexU2001 <77464396+AlexU2001@users.noreply.github.com>
2023-05-03 17:24:15 -0400

    Fixed small issues, animated fire

--- Project 1/Assets/Scripts/TabManager.cs
+++ Project 1/Assets/Scripts/TabManager.cs
@@ -4,20 +4,13 @@ public class TabManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] tabs;
 
-    private void Awake()
-    {
-        foreach (GameObject tab in tabs)
-        {
-            tab.SetActive(true);
-        }
-    }
-
     private void Start()
     {
         foreach (GameObject tab in tabs)
         {
             tab.SetActive(false);
         }
+        tabs[0].SetActive(true);
     }
 
     public void Select(GameObject target)
